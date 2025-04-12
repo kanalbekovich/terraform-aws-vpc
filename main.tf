baaf2fa2ca7dc8f5281b03a7ec37ab4fd7938cbf
kanalbekovich <kanalbekovich@gmail.com>
2025-04-12 16:12:01 +0000

    new module version

--- main.tf
+++ main.tf
@@ -15,6 +15,16 @@ resource "aws_subnet" "main1" {
   }
 }
 
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet2_cidr
+
+  tags = {
+    Name = "${var.environment}-subnet2"
+  }
+}
+
 variable vpc_cidr {}
 variable subnet1_cidr {}
+variable subnet2_cidr {}
 variable environment {}
